9812b298a0c9da2150d1388bbe173b108a81c4ef
memsharded <james@conan.io>
2020-12-02 13:52:43 +0100

    working

--- build.py
+++ build.py
@@ -1,25 +1,30 @@
 import os, shutil
 
+from jmetadata import JMetadata
+
 def run(cmd):
     ret = os.system(cmd)
     if ret != 0:
         raise Exception("ERROR running {}".format(cmd))
 
-def load(filepath):
-    with open(filepath, "rb") as f:
-        content = f.read()
-    return content
 
-#shutil.rmtree("build", ignore_errors=True)
+shutil.rmtree("build", ignore_errors=True)
 os.makedirs("build", exist_ok=True)
 os.chdir("build")
+metadata = JMetadata.load_json("../deps.json")
+metadata.generate_c()
+
 try:
-    #run('cmake .. -G "Visual Studio 15 Win64"')
-    run('cmake --build . --config Debug')
-    run(".\Debug\myapp.exe")
-    for f in ("myapp.exe", "mymath.lib", "myphsx.dll"):
-        app = load("Release\{}".format(f))
-        metadata = app.find(b"CARABOLO")
-        print("{} METADATA {}".format(f, metadata))
+    run('cmake .. -G "Visual Studio 15 Win64"')
+
+    build_types = "Release", "Debug"
+    for build_type in build_types:
+        run('cmake --build . --config {}'.format(build_type))
+        # Making sure the app works
+        run(".\{}\myapp.exe".format(build_type))
+        for f in ("myapp.exe", "mymath.lib", "myphsx.dll"):
+            file_metadata = JMetadata.load_binary("{}/{}".format(build_type, f))
+            assert file_metadata == metadata, "{}!={}".format(file_metadata, metadata)
+            print("{} metadata OK".format(f), file_metadata)
 finally:
     os.chdir("..")
---
+++ jmetadata.py
@@ -0,0 +1,55 @@
+import json
+
+metadata_h_template = """\
+#if WIN32
+__declspec(dllexport) extern char* mydata = "<jmetadata>{metadata}</jmetadata>";
+#else
+extern char* mydata = "<jmetadata>{metadata}</jmetadata>";
+#endif
+"""
+
+metadata_c = """\
+#include "jmetadata.h"
+"""
+
+def load(filepath):
+    with open(filepath, "rb") as f:
+        content = f.read()
+    return content
+
+
+class JMetadata:
+    def __init__(self, deps=None):
+        self._deps = deps
+
+    def __str__(self):
+        return str(self._deps)
+
+    def __eq__(self, other):
+        return self._deps == other._deps
+
+    def __ne__(self, other):
+        return self._deps != other._deps
+
+    @staticmethod
+    def load_json(filename):
+        deps = json.loads(load(filename))
+        return JMetadata(deps)
+
+    @staticmethod
+    def load_binary(filename):
+        content = load(filename)
+        metadata_start = content.find(b"<jmetadata>")
+        if metadata_start == -1:
+            return JMetadata()
+
+        metadata_end = content.find(b"</jmetadata>")
+        metadata_start += len("<jmetadata>")
+        metadata = content[metadata_start:metadata_end].decode("utf-8")
+        deps = metadata.split(",")
+        return JMetadata(deps)
+
+    def generate_c(self):
+        metadata_h = metadata_h_template.format(metadata=",".join(dep for dep in self._deps))
+        open("jmetadata.h", "w").write(metadata_h)
+        open("jmetadata.c", "w").write(metadata_c)
