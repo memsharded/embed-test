89331f1584dcc20bd754f7ae795c1c50416fec0c
memsharded <james@conan.io>
2020-12-02 14:19:39 +0100

    working in Linux too

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, platform
 
 from jmetadata import JMetadata
 
@@ -14,17 +14,28 @@ os.chdir("build")
 metadata = JMetadata.load_json("../deps.json")
 metadata.generate_c()
 
-try:
-    run('cmake .. -G "Visual Studio 15 Win64"')
+build_types = "Release", "Debug"
 
-    build_types = "Release", "Debug"
-    for build_type in build_types:
-        run('cmake --build . --config {}'.format(build_type))
-        # Making sure the app works
-        run(".\{}\myapp.exe".format(build_type))
-        for f in ("myapp.exe", "mymath.lib", "myphsx.dll"):
-            file_metadata = JMetadata.load_binary("{}/{}".format(build_type, f))
-            assert file_metadata == metadata, "{}!={}".format(file_metadata, metadata)
-            print("{} metadata OK".format(f), file_metadata)
+try:
+    if platform.system() == "Windows":
+        run('cmake .. -G "Visual Studio 15 Win64"')
+        for build_type in build_types:
+            run('cmake --build . --config {}'.format(build_type))
+            # Making sure the app works
+            run(".\{}\myapp.exe".format(build_type))
+            for f in ["myapp.exe", "mymath.lib", "myphsx.dll"]:
+                file_metadata = JMetadata.load_binary("{}/{}".format(build_type, f))
+                assert file_metadata == metadata, "{}!={}".format(file_metadata, metadata)
+                print("{} metadata OK".format(f), file_metadata)
+    else:
+        for build_type in build_types:
+            run('cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE={}'.format(build_type))
+            run('cmake --build .')
+            # Making sure the app works
+            run("./myapp")
+            for f in ["myapp", "libmymath.a", "libmyphsx.so"]:
+                file_metadata = JMetadata.load_binary("{}".format(f))
+                assert file_metadata == metadata, "{}!={}".format(file_metadata, metadata)
+                print("{} metadata OK".format(f), file_metadata)
 finally:
     os.chdir("..")
